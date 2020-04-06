69b8f599f94327ec8eb3ce3825e8aa37e6ecc763
Luca Passarella <luca@leva.io>
2020-04-06 19:08:14 +0200

    seem working!

--- autolevel.js
+++ autolevel.js
@@ -258,9 +258,7 @@ module.exports = class Autolevel {
                 ||
               (/(G38.+|G5.+|G10|G4.+|G92|G92.1)/gi.test(lineStripped)) // skip compensation for these G-Codes
             ){
-              result.push('( ORIGINAL )')
-              result.push(line)
-              result.push('\n')
+              result.push(lineStripped + '( ORIGINAL )')
             }
         else {
           if (/G91/i.test(lineStripped)) abs = false
@@ -281,10 +279,8 @@ module.exports = class Autolevel {
             for (let seg of segs) {
               let cpt = this.compensateZCoord(seg)
               // ( Z${seg.z.toFixed(this.decimals)} )`
-              let newLine = lineStripped + ` X${cpt.x.toFixed(this.decimals)} Y${cpt.y.toFixed(this.decimals)} Z${cpt.z.toFixed(this.decimals)}`
-              result.push(`( Z${seg.z.toFixed(this.decimals)} )`)
+              let newLine = lineStripped + ` X${cpt.x.toFixed(this.decimals)} Y${cpt.y.toFixed(this.decimals)} Z${cpt.z.toFixed(this.decimals)} ( Z${seg.z.toFixed(this.decimals)} )`
               result.push(newLine.trim())
-              result.push('\n')
             }
           } else {
             result.push(lineStripped)
